0b5cefc4fa3b85c87bf6a5365930066f93db6e42
Matthew R. Becker <becker.mr@gmail.com>
2022-04-19 12:40:45 -0500

    BUG name of script was wrong

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 # get the (de)activate scripts
-CHANGE="dectivate"
+CHANGE="deactivate"
 mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
 cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 

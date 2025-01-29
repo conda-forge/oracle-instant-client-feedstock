853b15e670f4c8d83e560a2cdb100ef17561df3a
idlefella <marc@liechti.one>
2025-01-29 18:02:16 +0100

    Update linux client to latest version and add option to download from different location (#6)
    * Update linux client to latest version
    
    Additionally add option to download the zip file from a different
    location.
    
    * Simplify version specification in meta.yaml
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.01.28.22.50.30
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.01.28.22.50.30
    
    * Update conda-forge.yml
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.01.28.22.50.30
    
    * Add detailed description to recipe meta.yaml
    
    * Update recipe/activate.sh
    
    * MNT: Re-rendered with conda-build 25.1.1, conda-smithy 3.45.4, and conda-forge-pinning 2025.01.28.22.50.30
    
    ---------
    
    Co-authored-by: Matthew R. Becker <beckermr@users.noreply.github.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,10 +13,16 @@ if [[ ${target_platform} == "osx-64" ]]; then
   sed -i.bak "s|@ARCH@|x64|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
   sed -i.bak "s|@OS@|macos|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
   sed -i.bak "s|@VERTAIL@|dbru|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+  aver=$(echo ${PKG_VERSION} | tr -d ".")
+  sed -i.bak "s|@AVER@|${aver}|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+
 elif [[ ${target_platform} == "linux-64" ]]; then
   sed -i.bak "s|@PLATFORM@|linux|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
   sed -i.bak "s|@ARCH@|x64|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
   sed -i.bak "s|@OS@|linux|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
-  sed -i.bak "s|@VERTAIL@|dbru|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+  sed -i.bak "s|@VERTAIL@||g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+  # the folder name for the version 23.7.0.25.1 is different than for the previous releases
+  aver=$(echo ${PKG_VERSION:0:6}.00.0 | tr -d ".")
+  sed -i.bak "s|@AVER@|${aver}|g" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 fi
 rm -f "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh.bak"
